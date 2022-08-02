9733157eb6307da7bc4e1aaf7b4eef62353d4784
MallikChikati <chikati.rao@gmail.com>
2022-08-02 06:07:04 -0400

    first commit

---
+++ src/main/java/com/cddish/utils/deploy/SpringAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package com.cddish.utils.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringAzureDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureDemoApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String message() {
+		return "Your spring application has been deployed onto Azure successfully";
+	}
+
+}
